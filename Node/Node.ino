3792959cbd70c2a78241f537f2fd5aa36316ca99
Sajin <code@saj.in>
2023-05-22 09:42:37 +0530

    mesh changes

--- Node/Node.ino
+++ Node/Node.ino
@@ -1,5 +1,25 @@
 #include "D:/Sru/smart-street-lights/Shared/Mesh.h"
 #include "Handlers.h"
 
+void SetupIO(){
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void SetupWifi() {
+  D("Client Board MAC Address: ");DL(WiFi.macAddress());
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  LookForSavedChannel();
+}
+
+void SetupEspNow() {
+  ESP_ERROR_CHECK(esp_wifi_set_channel(channel,  WIFI_SECOND_CHAN_NONE));
+  if(esp_now_init() != ESP_OK) DL("Error initializing ESP-NOW");
+  // set callback routines
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
 void setup() {}
 void loop() {}
